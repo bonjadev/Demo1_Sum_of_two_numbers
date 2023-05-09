cde4eff7c1a9804b2286307efeaae364ef840b66
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-05-09 11:30:01 +0200

    First commit

---
+++ Demo1_Sum_of_two_numbers.cpp
@@ -0,0 +1,17 @@
+// Demo1_Sum_of_two_numbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	int sum;
+	cout << "Type a number: ";
+	cin >> x;
+	cout << "Type another number: ";
+	cin >> y;
+	sum = x + y;
+	cout << "Sum is: " << sum;
+	return 0;
+}
